6c8a6ef801a7d4cc69acbaf2cd00edf5f349819d
RusGosuNagib <roman.vorobiev@ugasoft.com>
2024-03-02 13:56:03 +0300

    add key in @Headers

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -1,3 +1,3 @@
 import { EntityRepository } from '@mikro-orm/core';
-import { Users } from './User.entity';
+import { Users } from './user.entity';
 export class UserRepository extends EntityRepository<Users> {}
