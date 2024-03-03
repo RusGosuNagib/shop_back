dd6f4009eadcc5d7704b305292f5a714ecf227c0
RusGosuNagib <roman.vorobiev@ugasoft.com>
2024-03-03 21:58:39 +0300

    add some bugs

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors({
+    origin: 'http://185.117.154.177/',
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
